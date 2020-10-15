ba2067ff05527aa935c7a858e07c0c22a76c865b
duybach <duybach@live.de>
2020-10-15 19:43:57 +0200

    Add game functionality

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,6 +1,8 @@
 import React, { useCallback, useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
 import { connect } from 'react-redux';
 
+import Button from 'react-bootstrap/Button';
 import Col from 'react-bootstrap/Col';
 import Container from 'react-bootstrap/Container';
 import ListGroup from 'react-bootstrap/ListGroup';
@@ -11,6 +13,8 @@ import Chat from './Chat';
 import { setLobby } from '../actions';
 
 const Game = ({ socket, lobby, dispatch }) => {
+  const history = useHistory();
+
   const fetchLobby = useCallback(() => {
     socket.emit('lobbyStatus', lobby.id, (message) => {
       if ('error' in message) {
@@ -18,29 +22,78 @@ const Game = ({ socket, lobby, dispatch }) => {
       } else {
         dispatch(setLobby(message));
       }
-    });
+    })
 
     socket.on('lobbyStatus', (message) => {
       dispatch(setLobby(message));
     });
   }, [socket, dispatch, lobby.id]);
 
+  const pickPlayer = (playerId, action) => {
+    socket.emit('game', lobby.id, action, { playerId: playerId });
+  }
+
+  const renderActionButtons = (playerId) => {
+    if (lobby.players[socket.id].status !== 'PLAYER_ALIVE' || lobby.players[playerId].status !== 'PLAYER_ALIVE' || (lobby.game.phase === 1 && lobby.players[socket.id].role !== 'WERWOLF') || (lobby.game.phase === 2 && lobby.players[socket.id].role !== 'WITCH')) {
+      return (<></>);
+    }
+
+    if (lobby.game.phase === 0) {
+      return (
+        <>
+          {socket.id !== playerId ? <Button variant="danger" onClick={() => pickPlayer(playerId, 'PLAYER_VOTE')}>VOTE</Button> : ''}
+        </>
+      );
+    } else if (lobby.game.phase === 1 && lobby.players[socket.id].role === 'WERWOLF') {
+      return (
+        <>
+          {socket.id !== playerId ? <Button variant="danger" onClick={() => pickPlayer(playerId, 'PLAYER_KILL')}>KILL</Button> : ''}
+        </>
+      );
+    } else if (lobby.game.phase === 2 && lobby.players[socket.id].role === 'WITCH' && lobby.game.werwolfTarget === playerId) {
+      return (
+        <>
+          {socket.id !== playerId ? <Button variant="danger" onClick={() => pickPlayer(playerId, 'PLAYER_HEAL')}>HEAL</Button> : ''}
+        </>
+      );
+    } else {
+      return (<></>);
+    }
+  }
+
+  const tickTime = useCallback(() => {
+    lobby.game.timeLeft--;
+  }, [lobby.game.timeLeft]);
+
   useEffect(() => {
+    if (lobby.status === 'GAME_END') {
+      history.push(`/game_end/${lobby.id}`);
+    }
+
     fetchLobby();
-  }, [fetchLobby]);
+
+    let timerId = setInterval(tickTime, 1000);
+
+    return () => {
+      clearInterval(timerId);
+    };
+  }, [fetchLobby, history, lobby.id, lobby.status, tickTime]);
 
   return (
     <Container>
       <Row>
         <Col>
           <h1>Game {lobby.id}</h1>
-          <h3> Host: {lobby.hostId}</h3>
+          <h3>Host: {lobby.hostId}</h3>
+          <h3>Time: {lobby.game.phase === 0 ? 'day time' : 'night time'} ({lobby.game.round}.{lobby.game.phase})</h3>
+          <h3>Time left: {lobby.game.timeLeft}s</h3>
 
           <ListGroup>
             {
-              Object.keys(lobby.players).map((id, index) => (
-                <ListGroup.Item key={index}>
-                  {lobby.players[id].alias}
+              Object.keys(lobby.players).map((playerId, index) => (
+                <ListGroup.Item key={index} active={lobby.players[socket.id].targetPlayerId === playerId ? true : false}>
+                  {lobby.players[playerId].alias} ({lobby.players[playerId].role} | {lobby.players[playerId].status})
+                  {renderActionButtons(playerId)}
                 </ListGroup.Item>
               ))
             }
